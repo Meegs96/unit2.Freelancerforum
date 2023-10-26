19450cc8a21dd487145d47af4307b0b43aec8b93
Meegs96 <miguel.herrera.vindel96@gmail.com>
2023-10-26 14:59:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let freelancers = [
+    { name: 'Alice', price: 30, occupation: 'writer'},
+    { name: 'Bob', price: 50, occupation: 'teacher'},
+    { name: 'Carol', price: 70, occupation: 'programmer'},
+    { name: 'Tanner', price: 100. occupation: ''}
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
